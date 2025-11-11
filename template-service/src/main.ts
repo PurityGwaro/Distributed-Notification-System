ff7b7eaf9e586ebc9639c129f8204eb950e974f7
Jibola Paul <harjibbolar26@gmail.com>
2025-11-11 17:07:51 +0100

    feat: template service initialization

---
+++ template-service/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Template service API')
+    .setDescription('Template management service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  const port = process.env.PORT || 3004;
+  await app.listen(port);
+  console.log(`Templste service running on port ${port}`);
+}
+
+bootstrap();
