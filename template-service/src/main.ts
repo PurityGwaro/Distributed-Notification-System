1ebffb36dcd8ca7818f1c768611f08ebda7941e4
Jibola Paul <harjibbolar26@gmail.com>
2025-11-13 00:15:40 +0100

    fix for railway

--- template-service/src/main.ts
+++ template-service/src/main.ts
@@ -23,7 +23,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 3004;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Templste service running on port ${port}`);
 }
 
